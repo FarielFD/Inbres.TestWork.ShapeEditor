e3a8b3d9cf5e4bc16ba27b93180e2d67bcdaab1f
Alexandr Uglev <865135@mail.ru>
2025-11-13 01:53:44 +0300

    Установлены компоненты .NET SDK, создана библиотека GraphicShapeEditor.Core

---
+++ GraphicShapeEditor.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace GraphicShapeEditor.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Source/GraphicShapeEditor/Widgets/ShapesEditor/ShapesEditorWidget.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace GraphicShapeEditor;
+
+public partial class ShapesEditorWidget : UserControl
+{
+    public ShapesEditorWidget()
+    {
+        InitializeComponent();
+    }
+}
